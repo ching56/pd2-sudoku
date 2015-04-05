aa46672fad0ab5989be21ebc2bc6451c2d3bffc4
ching56 <F74036467@mail.ncku.edu.tw>
2015-04-05 13:37:26 +0800

    sudoku version.1 debugging

--- A.cpp
+++ A.cpp
@@ -6,13 +6,14 @@ class Sudoku{
 
 	public:
 		
-		Sudoku(){for(i=0;i<sudokuSize;++i)map[i]=0;};
-		Sudoku(const int init_map[]){for(i=0;i<sudokuSize;++i)map[i]=init_map[i];};
+		Sudoku(){for(int i=0;i<sudokuSize;++i)map[i]=0;temp_count=0;};
+		Sudoku(const int init_map[]){for(int i=0;i<sudokuSize;++i)map[i]=init_map[i];temp_count=0;};
 		
-		void ReadIn(){for(i=0;i<sudokuSize;++i)cin >> map[i];};
+		void ReadIn(){for(int i=0;i<sudokuSize;++i)cin >> map[i];};
 		void Solve(){
 			int judge = 0;
 			int ps = nextBlank(-1);
+			int i;
 			do{
 				map[ps]++;
 				if(map[ps] > 9){
@@ -21,14 +22,14 @@ class Sudoku{
 				}else{
 					if(check(ps) == true){
 						recordBlank(ps);
-						ps = nextBlanl(sp);
+						ps = nextBlank(ps);
 						if(ps == sudokuSize){
 							ps = previousBlank(ps);
 							judge++;
 						}
 					}
 				}
-			}while(ps >=  0 && ps < sudokuSize && judge < 2)
+			}while(ps >=  0 && ps < sudokuSize && judge < 2);
 			cout << judge << endl;
 			for(i = 0;i < sudokuSize;++i){
 				cout << map[i];
@@ -40,26 +41,31 @@ class Sudoku{
 
 	private:
 
-		void recordBlank(int ps){tmep_ps[temp_count++]=ps;};
+		int temp_ps[sudokuSize];
+		int temp_count; 
+		int map[sudokuSize];
+		
+		void recordBlank(int ps){temp_ps[temp_count++]=ps;};
 		int nextBlank(int ps){
 			do{ps++;}while( ps < sudokuSize && map[ps] > 0);
 			return ps;
 		};
 		int previousBlank(int ps){
 			if(temp_count < 0)return -1;
-			ps = temp_ps[--tmep_count];
+			ps = temp_ps[--temp_count];
 		};
-		void findstart_row(int ps){
+		int findstart_row(int ps){
 			return (ps - ( ps % 12 ) + 1);
 		};
-		void findstart_col(int ps){
-			return ( ps % 12 )
+		int findstart_col(int ps){
+			return ( ps % 12 );
 		};
-		void findstart_blo(int ps){
-			return ( 1+(ps/36)*36+ps%12/3*3)
+		int findstart_blo(int ps){	
+			return ( 1+(ps/36)*36+ps%12/3*3);
 		};
 		bool checkUnity(int arr[]){
-			int arr[]_unity[9];
+			int arr_unity[9];
+			int i;
 			for(i = 0;i < 9;i++)
 				arr_unity[i]=0;
 			for(i = 0;i < 12;i++){
@@ -67,13 +73,14 @@ class Sudoku{
 					++arr_unity[arr[i]-1];
 			}
 			for(i = 0;i < 9;i++)
-				if(arr_unity[i] != 1)
+				if(arr_unity[i] > 1)
 					return false;
 			return true;	
 		};
 		bool check(int ps){
 			int check_arr[12];
 			int start;
+			int i;
 			//check rows
 			start = findstart_row(ps);
 			for(i=0;i<12;i++)
@@ -91,7 +98,4 @@ class Sudoku{
 			if(checkUnity(check_arr) == false)return false;
 			return true;
 		}
-		int temp_ps[sudokuSize];
-		int temp_count = 0; 
-		int map[sudokuSize];
 };

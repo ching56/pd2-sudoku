fe05c5d4658f70dbf852e8429d7d60460f72a3c1
ching56 <F74036467@mail.ncku.edu.tw>
2015-04-05 21:53:16 +0800

    version.1 debuged(previos commit has stdout error )

--- A.cpp
+++ A.cpp
@@ -8,17 +8,18 @@ class Sudoku{
 
 	public:
 		
-		Sudoku(){for(int i=0;i<sudokuSize;++i){map[i]=0;temp_ps[i]=0;temp_value[i]=0;}temp_count=0;};
-		Sudoku(const int init_map[]){for(int i=0;i<sudokuSize;++i){map[i]=init_map[i];temp_ps[i]=0;temp_value[i]=0;}temp_count=0;};
+		Sudoku(){for(int i=0;i<sudokuSize;++i){map[i]=0;temp_ps[i]=0;ans[i]=0;}temp_count=0;};
+		Sudoku(const int init_map[]){for(int i=0;i<sudokuSize;++i){map[i]=init_map[i];temp_ps[i]=0;ans[i]=0;}temp_count=0;};
 		
-		void ReadIn(){for(int i=0;i<sudokuSize;++i)cin >> map[i];};
+		void ReadIn(){for(int i=0;i<sudokuSize;++i){cin >> map[i];ans[i]=map[i];};};
 		void Solve(){
 			int judge = 0;
 			int ps = nextBlank(-1);
 			int i;
 			int counter=0;
-			do{	//test
-				cout<<counter<<endl;counter++;
+			do{	
+				//test
+				counter++;
 				map[ps]++;
 				if(map[ps] > 9){
 					map[ps] = 0;
@@ -30,16 +31,18 @@ class Sudoku{
 						if(ps == sudokuSize){
 							ps = previousBlank(ps);
 							judge++;
+							for(i=0;i<sudokuSize;i++)ans[i]=map[i];
 						}
 					}
 				}
 			}while(ps >=  0 && ps < sudokuSize && judge < 2);
 			cout << judge << endl;
-			recoverBlank();
 			for(i = 0;i < sudokuSize;++i){
-				cout << map[i]<<' ';
+				cout << ans[i]<<' ';
 				if((i+1) % 12 == 0)cout << endl;
 			}
+			//test
+			cout<<"do "<<counter<<" times"<<endl;
 		};
 		void GiveQuestion();
 		static const int sudokuSize = 144;
@@ -48,10 +51,9 @@ class Sudoku{
 
 		int temp_ps[sudokuSize];
 		int temp_count;
-		int temp_value[sudokuSize];
-		int map[sudokuSize];
-		
-		void recordBlank(int ps){temp_value[temp_count]=map[ps];temp_ps[temp_count++]=ps;};
+		int ans[sudokuSize];
+		int map[sudokuSize];		
+		void recordBlank(int ps){temp_ps[temp_count++]=ps;};
 		int nextBlank(int ps){
 			do{ps++;}while( ps < sudokuSize && map[ps] > 0 || map[ps] == -1);
 			return ps;
@@ -60,10 +62,6 @@ class Sudoku{
 			if(temp_count == 0)return -1;
 			ps = temp_ps[--temp_count];
 			return ps;
-		};
-		void recoverBlank(){
-			int i=0;
-			while(temp_value[i] !=0 ){map[temp_ps[i]]=temp_value[i];i++;}
 		};
 		int findstart_row(int ps){
 			return (ps - ( ps % 12 ));
@@ -79,6 +77,7 @@ class Sudoku{
 			int i;
 			for(i = 0;i < 9;i++)
 				arr_unity[i]=0;
+			
 			for(i = 0;i < 12;i++){
 				if(arr[i] != -1 )
 					++arr_unity[arr[i]-1];
@@ -88,26 +87,29 @@ class Sudoku{
 					return false;
 			return true;	
 		};
+		
 		bool check(int ps){
 			int check_arr[12];
-			int start;
+			int row_start;
+			int col_start;
+			int blo_start;
 			int i;
 			for(i=0;i<12;++i)check_arr[i]=0;
 			//check rows
-			start = findstart_row(ps);
+			row_start = findstart_row(ps);
 			for(i=0;i<12;i++)
-				check_arr[i] = map[start + i];
+				check_arr[i] = map[row_start + i];
 			if(checkUnity(check_arr) == false){return false;};
 			//check colums
-			start = findstart_col(ps);
+			col_start = findstart_col(ps);
 			for(i=0;i<12;i++)
-				check_arr[i] = map[start+i*12];
+				check_arr[i] = map[col_start+i*12];
 			if(checkUnity(check_arr) == false){return false;};
 			//check block
-			start = findstart_blo(ps);
+			blo_start = findstart_blo(ps);
 			for(i=0;i<12;i++)check_arr[i]=0;
 			for(i=0;i<9;i++)
-				check_arr[i] = map[start+i/3*12+i%3];
+				check_arr[i] = map[blo_start+i/3*12+i%3];
 			if(checkUnity(check_arr) == false){return false;};
 			return true;
 		}
